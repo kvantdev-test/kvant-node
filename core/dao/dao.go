7d703b7b72df6530effc4b73e5348f1d783b0ea8
KvantDev <kvantdev-test@kvant.io>
2020-02-23 17:56:57 +0300

    v1.2.02

--- core/dao/dao.go
+++ core/dao/dao.go
@@ -4,5 +4,5 @@ import "github.com/kvant-node/core/types"
 
 var (
 	Address    = types.HexToAddress("Mx18467bbb64a8edf890201d526c35957d82be3d95")
-	Commission = 10
+	Commission = 0
 )
--- core/developers/developers.go
+++ core/developers/developers.go
@@ -4,5 +4,5 @@ import "github.com/kvant-node/core/types"
 
 var (
 	Address    = types.HexToAddress("Mx04bea23efb744dc93b4fda4c20bf4a21c6e195f1")
-	Commission = 10
+	Commission = 0
 )
